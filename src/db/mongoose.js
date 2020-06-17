4bcb96db8700326cbb52842f7f1ac39105a63cff
Debasis Jana <debasisjana93@gmail.com>
2020-06-17 20:39:53 +0530

    Added resource reading function to read tasks and users

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,28 +1,6 @@
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
-    useNewUrlParser:true,
-    useCreateIndex:true,
-})
-const Task = mongoose.model('Task',{
-    description :
-    {
-        type:String,
-        required:true,
-        trim:true,
-    },
-    completed:{
-        type:Boolean,
-        default:false,
-    }
-})
+    useNewUrlParser : true,
+    useCreayeIndex : true,
 
-/*const task =new Task({
-    description:'Learn Nodejs',
-    completed:false,
 })
-
-task.save().then(()=>{
-    console.log(task);
-}).catch((error)=>{
-    console.log("Error",error);
-})*/
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express =require('express')
 require('./db/mongoose')
 const User=require('./models/user')
+const Task = require('./models/task')
 const app =express()
 const port =process.env.PORT || 3000
 
@@ -12,6 +13,54 @@ app.post('/users',(req,res)=> {
         res.send(user);
     }).catch(() => {
         res.status(400).send(e);
+    })
+})
+//reading all data
+app.get('/users',(req,res)=>{
+    User.find({}).then((users)=>{
+        res.send(users)
+    }).catch((e)=>{
+
+    })
+})
+//reading data by id
+app.get('/users/:id',(req,res)=>{
+    const _id=req.params._id
+    User.findById(_id).then((user)=>{
+        if(!user){
+            return res.status(404).send()
+        }
+        res.send(user)
+    }).catch((e)=>{
+        res.status(400).send()
+    })
+})
+app.post('/tasks',(req,res)=>{
+    const task=new Task(req.body)
+    task.save().then(()=>{
+        res.send(task);
+    }).catch(()=>{
+        res.status(400).send(e);
+    })
+})
+//reading all data
+app.get('/tasks',(req,res)=>{
+    Task.find({}).then((tasks)=>{
+        res.send(tasks)
+    }).catch((e)=>{
+
+    })
+})
+//reading data by id
+app.get('/tasks/:id',(req,res)=>{
+    const _id=req.params._id
+    User.findById(_id).then((task)=>{
+        if(!task){
+            return res.status(404).send()
+        }
+        res.send(task)
+    }).catch((e)=>{
+        res.status(400).send()
     })
 })
 app.listen(port,()=>{
