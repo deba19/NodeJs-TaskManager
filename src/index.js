39f0ea3a39b38505f24303b283e81854e508ed79
Debasis Jana <debasisjana93@gmail.com>
2020-06-29 11:46:47 +0530

    Added middleware for site maintenance mode

--- src/index.js
+++ src/index.js
@@ -7,9 +7,13 @@ const userRouter=require('./routers/user')
 const taskRouter=require('./routers/task')
 const port =process.env.PORT || 3000
 
+app.use((req,res,next)=>{
+    res.status(503).send('Site is curretly down. Check after sometime')
+ })
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
+
 
 app.listen(port,()=>{
     console.log("Server is up on port "+port);
