bcb90944834b4c67d1d69e7969d4ccfff3c59d07
Debasis Jana <debasisjana93@gmail.com>
2020-06-29 21:00:39 +0530

    added module to logout from single device and logout from all devices

--- src/routers/user.js
+++ src/routers/user.js
@@ -33,6 +33,31 @@ router.post('/users/login',async(req,res)=>{
         res.status(400).send()
     }
 })
+//Logout user , single device
+router.post('/users/logout',auth,async(req,res)=>{
+    try
+    {
+        req.user.tokens=req.user.tokens.filter((token)=>{
+            return token.token!== req.token
+        })
+        await req.user.save()
+        res.send()
+    }catch(e)
+    {
+        res.status(500).send()
+    }
+})
+// Logout from all device
+router.post('/users/logoutAll',auth,async(req,res)=>{
+    try{
+        req.user.tokens=[]
+        await req.user.save()
+        res.send()
+    }catch(e)
+    {
+        res.status(500).send()
+    }
+})
 //reading all data
 router.get('/users/me',auth,async(req,res)=>{
     res.send(req.user)
